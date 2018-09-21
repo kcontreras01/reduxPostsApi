9fb759c81a008ac3cf94176098a06351d1a15ec9
Kiara Contreras <kiara@hashtag-labs.com>
2018-09-21 09:52:18 -0400

    redux working

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {createPost} from '../actions/PostActions';
 
 class PostForm extends Component {
     constructor(props) {
@@ -22,14 +25,7 @@ class PostForm extends Component {
             body: this.state.body
         };
 
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            headers:{
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(post)
-        }).then(res => res.json())
-        .then(data => console.log(data));
+        this.props.createPost(post);
     }
 
     render() {
@@ -55,4 +51,8 @@ class PostForm extends Component {
     }
 }
 
-export default PostForm;
+PostForm.PropTypes = {
+  createPost: PropTypes.func.isRequired
+}
+
+export default connect(null, {createPost})(PostForm);
